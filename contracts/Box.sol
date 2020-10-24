e4bc552a25d584d7ee374ece70fffca0294e5be4
Sid Raj <siddharth.rajesh13@gmail.com>
2020-10-24 11:43:58 +1100

    Box inherits from Ownable contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,13 +1,28 @@
 pragma solidity ^0.7.2;
 
-contract Box {
+// import Auth contract
+import "./access-control/Auth.sol";
+
+// import Ownable from OZ contracts
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Box contract inherits from Ownable contract
+contract Box is Ownable {
   uint256 private value;
+  Auth private auth;
 
   // event emitted when stored value changes
   event ValueChanged(uint256 newValue);
 
+  constructor(Auth _auth) public {
+    auth = _auth;
+  }
+
   // store new value in contract
   function store(uint256 newValue) public {
+    // require caller of contract function is administrator in Auth
+    require(auth.isAdministrator(msg.sender), "Unauthorized");
+
     value = newValue;
     emit ValueChanged(newValue);
   }
